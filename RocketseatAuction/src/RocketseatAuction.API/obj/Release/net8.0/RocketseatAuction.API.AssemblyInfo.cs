2dcb73c6a77ffb769fda819f0e8cb13b2432b6bd
Pedro Henrique Dornela Gonçalves <pedro.henriquedgoncalves@gmail.com>
2024-02-09 18:20:21 -0300

    Auction Project

--- RocketseatAuction/src/RocketseatAuction.API/obj/Release/net8.0/RocketseatAuction.API.AssemblyInfo.cs
+++ RocketseatAuction/src/RocketseatAuction.API/obj/Release/net8.0/RocketseatAuction.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RocketseatAuction.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36286c1b45c1d24dd25b34ec6685333e7c51891f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RocketseatAuction.API")]
